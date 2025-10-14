2409b1943b6d10a191558123f295916a6220a728
Matthew Taylor <seleukos1209@googlemail.com>
2025-10-14 12:40:30 +0100

    updated setup script to handle openssl error on windows

--- setup.R
+++ setup.R
@@ -1,3 +1,11 @@
+# Install/activate renv
 if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv")
 
-renv::restore(prompt=FALSE)
+# Prefer Posit Package Manager (more reliable binaries)
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/latest"))
+
+# On macOS, prefer binaries (Windows uses binaries by default)
+if (Sys.info()[["sysname"]] == "Darwin") options(pkgType = "binary")
+
+# Restore the project library
+renv::restore(prompt = FALSE)
