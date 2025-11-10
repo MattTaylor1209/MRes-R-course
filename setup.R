faa49deddc8c8d30843c0143766df8cfd4a69a84
Matthew Taylor <seleukos1209@googlemail.com>
2025-11-10 16:22:38 +0000

    Updated setup script to account for packages which need installing from source

--- setup.R
+++ setup.R
@@ -6,24 +6,43 @@ required_packages <- c("tidyverse","arrow","babynames","curl","ggrepel","ggridge
                        "limma","edgeR","Rsubread","biomaRt","pheatmap","clusterProfiler")
 
 
-# Install BiocManager if needed
+# Ensure binaries only (good for students without RTools)
+options(pkgType = "binary")
+options(install.packages.compile.from.source = "never")
+options(install.packages.check.source = "no")
+
+# Make sure Bioconductor repos are used correctly
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
-  install.packages("BiocManager")
+  install.packages("BiocManager", type = "binary")
 }
+
 
 # Try BiocManager first, then fallback to install.packages
 for (pkg in required_packages) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     message("Trying BiocManager::install('", pkg, "')")
     tryCatch({
-      BiocManager::install(pkg, ask = FALSE, update = FALSE)
+      BiocManager::install(pkg, update = FALSE)
     }, error = function(e_bioc) {
       message("BiocManager failed. Trying install.packages('", pkg, "')")
       tryCatch({
-        install.packages(pkg, dependencies = TRUE)
+        install.packages(pkg, dependencies = TRUE, update = FALSE, type = "binary")
       }, error = function(e_cran) {
         message("Failed to install '", pkg, "' via both methods.")
       })
+    })
+  }
+}
+
+source_packages <- c("org.Rn.eg.db", "GO.db")
+
+for (pkg in source_packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    message("Trying BiocManager::install('", pkg, "')")
+    tryCatch({
+      BiocManager::install(pkg, type = "source", update = FALSE)
+    }, error = function(e_bioc) {
+      message("BiocManager failed")
     })
   }
 }
